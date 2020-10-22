e28f25c8cef0f887edf3f3431ca03a2158d83508
es174 <73078499+es174@users.noreply.github.com>
2020-10-22 21:29:47 +0300

    Add files via upload

---
+++ 1.1b.cpp
@@ -0,0 +1,71 @@
+﻿
+#include <iostream>
+#include <cmath>
+using namespace std;
+void create(int* x, int n)
+{
+	cout << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << "x[" << i << "] =";
+		cin >> x[i];
+	}
+}
+void show(int* x, int n)
+{
+	for (int i = 0; i < n; i++)
+		cout << "x[" << i << "] = " << x[i] << endl;
+}
+int main()
+{
+	int  n, i;
+	double t, x, b, j, odin, dva, h, F;
+	odin = 999999999.0;
+	dva = 0.0;
+	cout << "Vvedite a1, a2, a3: " << endl;
+	double *a = new double[3];
+	for ( i = 0; i < 3; i++)
+	{
+		cout << "a[" << i+1 << "] =";
+		cin >> a[i];
+	}
+	cout << "Vvedite b: ";
+	cin >> b;
+	cout << "Vvedite h: ";
+	cin >> h;
+	cout << "Vvedite n: ";
+	cin >> n;
+	for ( i = 0; i < 3; i++)
+	{
+		if (a[i] >= 0)
+		{
+			for (j = 1; j <= n; j++)
+			{
+				x = b + j * h;
+				t = a[i] - x + 1.0;
+				F = x * cos(t);
+				if (abs(F) < odin)
+					odin = abs(F);
+				if (abs(F) > dva)
+					dva = abs(F);
+			}
+		}
+
+		else
+		{	
+			for (j = 1; j <= n; j++)
+			{
+				x = b + j * h;
+				t = a[i] + x;
+				F = 2 * cos(t);
+				if (abs(F) < odin)
+					odin = abs(F);
+				if (abs(F) > dva)
+					dva = abs(F);
+			}
+		}
+	}
+	cout << "Chislo odin = " << odin << endl << "Chislo dva = " << dva;
+		return 0;
+		system("pause");
+}
