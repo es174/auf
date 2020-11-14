580d284e3778b348b9a9b3958f8f54f447eb1454
es174 <73078499+es174@users.noreply.github.com>
2020-11-14 16:13:17 +0300

    KONTRHOHA 2

---
+++ kontroha_2_zadanie_1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int a, b, c, d, i;
+	bool t;
+	t = false;
+	cin >> a >> b >> c >> d;
+	for (i = 10000; i < 99999; i++)
+	{
+		if (i % a == b and i % c == d)
+		{
+			cout << i << " ";
+			t = true;
+		}
+	}
+	if (!t)
+		cout << "-1";
+}
